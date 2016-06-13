b094d6d5ce60ce682969a5bc416ebb13b5598c14
miguelc95 <miguelc.95@hotmail.com>
2016-06-13 16:21:36 -0500

    k

---
+++ Event ItBasesDatos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Event ItBasesDatos
+//
+//  Created by Miguel Cuellar on 5/4/16.
+//  Copyright © 2016 Miguel Cuellar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
